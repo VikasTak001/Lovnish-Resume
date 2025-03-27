c0bc6b31566a80df571fcdfd765e1ab517bc4a36
Vikas Tak <vikastak0011@gmail.com>
2025-03-27 20:00:11 +0530

    Updated By Vikas

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 const body = document.querySelector('body');
 const icons = document.querySelectorAll('.icon');
 const topright = document.querySelector('.top-right');
-icons.forEach(icon => {
+icons.forEach (icon => {  
   icon.addEventListener('click', (event) => {
     icon.classList.toggle("open");
     body.classList.toggle('unscroll');
     topright.classList.toggle('top-right-show');
   });
+});
+document.querySelectorAll('.new-tab').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default behavior
+        window.open(link.href, '_blank'); // Open in a new tab
+    });
 });
