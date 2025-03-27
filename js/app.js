fb49c0c4178cd00e1a6990deb7778a3f21ba035f
Vikas Tak <vikastak0011@gmail.com>
2025-03-27 17:00:41 +0530

    Updated By Vikas

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body');
 const icons = document.querySelectorAll('.icon');
 const topright = document.querySelector('.top-right');
-icons.forEach (icon => {  
+icons.forEach(icon => {
   icon.addEventListener('click', (event) => {
     icon.classList.toggle("open");
     body.classList.toggle('unscroll');
